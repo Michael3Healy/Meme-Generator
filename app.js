7b3550b3e71a9b084c06ab4a4c23ed024fe6a33b
Prosper Arineitwe Asiimwe <prox0591@gmail.com>
2024-02-06 00:57:14 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,93 +1,122 @@
-const imageInput = document.querySelector('#image');
-const topTextInput = document.querySelector('#top-text');
-const bottomTextInput = document.querySelector('#bottom-text');
-const fontSizeInput = document.querySelector('#font-size');
-const form = document.querySelector('#meme-form');
-const list = document.querySelector('#meme-list');
+// Retrieve DOM elements
+const imageInput = document.querySelector("#image");
+const topTextInput = document.querySelector("#top-text");
+const bottomTextInput = document.querySelector("#bottom-text");
+const fontSizeInput = document.querySelector("#font-size");
+const form = document.querySelector("#meme-form");
+const list = document.querySelector("#meme-list");
 
-function hasInputValue (input) {
-    if (input.value !== '') {
-        return true;
-    }
-    return false;
+// Check if input has value
+function hasInputValue(input) {
+  return input.value.trim() !== "";
 }
 
-function resetInput (inputs) {
-    for (let input of inputs) {
-        input.value = '';
-    }
+// Reset input fields
+function resetInput(inputs) {
+  inputs.forEach((input) => (input.value = ""));
 }
 
-const storedMemes = JSON.parse(localStorage.getItem('creations')) || [];
+// Retrieve stored memes from local storage or initialize empty array
+const storedMemes = JSON.parse(localStorage.getItem("creations")) || [];
 
-for (let meme of storedMemes) {
-    let newMeme = document.createElement('div');
-    newMeme.setAttribute('class', 'meme');
-    list.append(meme)
+// Create memes from stored data
+storedMemes.forEach((meme) => {
+  const newMeme = document.createElement("div");
+  newMeme.setAttribute("class", "meme");
 
-    const memeImage = document.createElement('img');
-    memeImage.setAttribute('src', meme.meme);
+  const memeImage = document.createElement("img");
+  memeImage.setAttribute("src", meme.meme);
 
-    const removeButton = document.createElement('button');
-    removeButton.setAttribute('class', 'button-overlay');
-    removeButton.innerHTML = '<img width="32" height="32" src="http://tinyurl.com/yvfpll9n" alt="trash"/>';
+  const removeButton = document.createElement("button");
+  removeButton.setAttribute("class", "button-overlay");
+  removeButton.innerHTML =
+    '<img width="32" height="32" src="http://tinyurl.com/yvfpll9n" alt="trash"/>';
 
-    const topText = document.createElement('h1');
-    topText.innerText = meme.tText;
-    topText.setAttribute('class', 'top-text');
-    topText.style.fontSize = meme.fontSize;
+  const topText = document.createElement("h1");
+  topText.innerText = meme.tText;
+  topText.setAttribute("class", "top-text");
+  topText.style.fontSize = meme.fontSize;
 
-    const bottomText = document.createElement('h1');
-    bottomText.innerText = bText;
-    bottomText.setAttribute('class', 'bottom-text');
-    bottomText.style.fontSize = meme.fontSize;
+  const bottomText = document.createElement("h1");
+  bottomText.innerText = meme.bText;
+  bottomText.setAttribute("class", "bottom-text");
+  bottomText.style.fontSize = meme.fontSize;
 
-    newMeme.append(memeImage, removeButton, topText, bottomText);
+  newMeme.append(memeImage, removeButton, topText, bottomText);
+  list.append(newMeme);
+});
 
-}
+// Event listener for form submission
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (hasInputValue(imageInput) && hasInputValue(topTextInput) && hasInputValue(bottomTextInput) && hasInputValue(fontSizeInput)) {
-        const meme = document.createElement('div');
-        meme.setAttribute('class', 'meme');
-        list.append(meme)
+  // Check if all input fields are filled
+  if (
+    hasInputValue(imageInput) &&
+    hasInputValue(topTextInput) &&
+    hasInputValue(bottomTextInput) &&
+    hasInputValue(fontSizeInput)
+  ) {
+    // Create meme div
+    const meme = document.createElement("div");
+    meme.setAttribute("class", "meme");
 
-        const memeImage = document.createElement('img')
-        memeImage.setAttribute('src', imageInput.value);
+    // Create meme image
+    const memeImage = document.createElement("img");
+    memeImage.setAttribute("src", imageInput.value);
 
-        const removeButton = document.createElement('button');
-        removeButton.setAttribute('class', 'button-overlay');
-        removeButton.innerHTML = '<img width="32" height="32" src="http://tinyurl.com/yvfpll9n" alt="trash"/>';
-        
+    // Create remove button
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "button-overlay");
+    removeButton.innerHTML =
+      '<img width="32" height="32" src="http://tinyurl.com/yvfpll9n" alt="trash"/>';
 
-        const topText = document.createElement('h1');
-        topText.innerText = topTextInput.value;
-        topText.setAttribute('class', 'top-text');
-        topText.style.fontSize = fontSizeInput.value + 'px';
+    // Create top text
+    const topText = document.createElement("h1");
+    topText.innerText = topTextInput.value;
+    topText.setAttribute("class", "top-text");
+    topText.style.fontSize = fontSizeInput.value + "px";
 
-        const bottomText = document.createElement('h1');
-        bottomText.innerText = bottomTextInput.value;
-        bottomText.setAttribute('class', 'bottom-text');
-        bottomText.style.fontSize = fontSizeInput.value + 'px';
+    // Create bottom text
+    const bottomText = document.createElement("h1");
+    bottomText.innerText = bottomTextInput.value;
+    bottomText.setAttribute("class", "bottom-text");
+    bottomText.style.fontSize = fontSizeInput.value + "px";
 
-        meme.append(memeImage, removeButton, topText, bottomText);
+    // Append elements to meme div
+    meme.append(memeImage, removeButton, topText, bottomText);
+    list.append(meme);
 
-        storedMemes.push({ meme: memeImage.getAttribute('src'), tText: topText.innerText, bText: bottomText.innerText, fontSize: topText.style.fontSize});
-        localStorage.setItem('creations', JSON.stringify(storedMemes));
+    // Store meme data in local storage
+    storedMemes.push({
+      meme: imageInput.value,
+      tText: topTextInput.value,
+      bText: bottomTextInput.value,
+      fontSize: fontSizeInput.value,
+    });
+    localStorage.setItem("creations", JSON.stringify(storedMemes));
 
-        resetInput([imageInput, topTextInput, bottomTextInput]);
+    // Reset input fields
+    resetInput([imageInput, topTextInput, bottomTextInput]);
+  } else {
+    alert("You must fill out every field");
+  }
+});
 
-    } else {
-        alert('You must fill out every field')
+// Event listener for removing memes
+list.addEventListener("click", function (e) {
+  const trashIcon = e.target.closest('img[src="http://tinyurl.com/yvfpll9n"]');
+  if (trashIcon) {
+    trashIcon.parentElement.parentElement.remove();
+    // Remove meme from storedMemes array
+    const index = storedMemes.findIndex(
+      (meme) =>
+        meme.meme ===
+        trashIcon.parentElement.parentElement.querySelector("img").src
+    );
+    if (index !== -1) {
+      storedMemes.splice(index, 1);
+      localStorage.setItem("creations", JSON.stringify(storedMemes));
     }
-})
-
-list.addEventListener('click', function (e) {
-    const trashIcon = document.querySelector('img[src="http://tinyurl.com/yvfpll9n"]')
-    if (e.target.tagName === 'BUTTON') {
-        e.target.parentElement.remove();
-    } else if (e.target === trashIcon) {
-        e.target.parentElement.parentElement.remove();
-    }
-})
+  }
+});
